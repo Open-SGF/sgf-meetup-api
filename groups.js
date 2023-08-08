03514f0d663ca710627057d831bf6ffce13a36cf
jugglingdev <kaylamarie1785@gmail.com>
2023-08-08 16:15:40 -0500

    Create new file with query to grab multiple events by group in single API call

---
+++ groups.js
@@ -0,0 +1,176 @@
+const meetupAccessToken = 'YOUR_MEETUP_ACCESS_TOKEN';
+const meetupGraphQlEndpoint = 'https://api.meetup.com/gql';
+const batchSize = 10; // Number of events to fetch in each batch
+
+const GET_FUTURE_EVENTS = `
+query ($itemsNum: Int!, $cursor: String) {
+    group1Events: groupByUrlname(urlname: "open-sgf") {
+      unifiedEvents(input: { first: $itemsNum, after: $cursor }) {
+        count
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        edges {
+          node {
+            title
+            eventUrl
+            description
+            dateTime
+            duration
+            venue {
+              name
+              address
+              city
+              state
+              postalCode
+            }
+            group {
+              name
+              urlname
+            }
+            host {
+              name
+            }
+            images {
+              baseUrl
+              preview
+            }
+          }
+        }
+      }
+    }
+    
+    group2Events: groupByUrlname(urlname: "springfield-women-in-tech") {
+      unifiedEvents(input: { first: $itemsNum, after: $cursor }) {
+        count
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        edges {
+          node {
+            title
+            eventUrl
+            description
+            dateTime
+            duration
+            venue {
+              name
+              address
+              city
+              state
+              postalCode
+            }
+            group {
+              name
+              urlname
+            }
+            host {
+              name
+            }
+            images {
+              baseUrl
+              preview
+            }
+          }
+        }
+      }
+    }
+    
+    group3Events: groupByUrlname(urlname: "sgfdevs") {
+      unifiedEvents(input: { first: $itemsNum, after: $cursor }) {
+        count
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        edges {
+          node {
+            title
+            eventUrl
+            description
+            dateTime
+            duration
+            venue {
+              name
+              address
+              city
+              state
+              postalCode
+            }
+            group {
+              name
+              urlname
+            }
+            host {
+              name
+            }
+            images {
+              baseUrl
+              preview
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+async function fetchAllFutureEvents(urlname, cursor = null) {
+  const requestBody = JSON.stringify({
+    query: GET_FUTURE_EVENTS,
+    variables: {
+      urlname,
+      itemsNum: batchSize,
+      cursor,
+    },
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${meetupAccessToken}`,
+    },
+    body: requestBody,
+  };
+
+  try {
+    const response = await fetch(meetupGraphQlEndpoint, requestOptions);
+    const data = await response.json();
+    const events = data.data.proNetworkByUrlname.eventsSearch.edges;
+
+    if (data.data.proNetworkByUrlname.eventsSearch.pageInfo.hasNextPage) {
+      const nextCursor = data.data.proNetworkByUrlname.eventsSearch.pageInfo.endCursor;
+      const nextEvents = await fetchAllFutureEvents(urlname, nextCursor);
+      events.push(...nextEvents);
+    }
+
+    return events;
+    
+  } catch (error) {
+      console.error('Error fetching future events:', error);
+      return [];
+  }
+}
+
+async function fetchAndPrintAllFutureEvents(urlname) {
+  const futureEvents = await fetchAllFutureEvents(urlname);
+
+  futureEvents.forEach(event => {
+    console.log('--------------');
+    console.log('Event:', event.node.title);
+    console.log('Event Link:', event.node.eventUrl);
+    console.log('Description:', event.node.description);
+    console.log('Time:', new Date(event.node.dateTime));
+    console.log('Duration:', event.node.duration);
+    console.log('Location:', event.node.venue.name + '\n' + event.node.venue.address + '\n' + event.node.venue.city + ', ' + event.node.venue.state + '\n' + event.node.venue.postalCode);
+    console.log('Group:', event.node.group.name);
+    console.log('--------------');
+  });
+}
+
+const proNetworkUrlname = 'YOUR_PRO_NETWORK_URLNAME';
+fetchAndPrintAllFutureEvents(proNetworkUrlname);
+  
