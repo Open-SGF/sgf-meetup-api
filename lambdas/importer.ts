62cb3a5606e4d3185fec8967adf2d24de56393b1
Levi Zitting <me@levizitting.com>
2023-10-22 22:42:57 -0500

    chore: add working eslint config and fix errors

--- lambdas/importer.ts
+++ lambdas/importer.ts
@@ -1,15 +1,14 @@
 import 'dotenv/config';
 import fetch from 'node-fetch';
-import {getMeetupToken} from "./lib/get-meetup-token";
+import { getMeetupToken } from './lib/get-meetup-token';
 
+async function fetchEvents(meetupAccessToken: string) {
+	const meetupGraphQlEndpoint = 'https://api.meetup.com/gql';
+	const batchSize = 10; // Number of events to fetch in each batch
 
-async function fetchEvents(meetupAccessToken) {
-    const meetupGraphQlEndpoint = 'https://api.meetup.com/gql';
-    const batchSize = 10; // Number of events to fetch in each batch
+	const GROUP_URLNAMES = ['open-sgf', 'springfield-women-in-tech', 'sgfdevs'];
 
-    const GROUP_URLNAMES = ['open-sgf', 'springfield-women-in-tech', 'sgfdevs'];
-
-    const GET_FUTURE_EVENTS = `
+	const GET_FUTURE_EVENTS = `
       query ($urlname: String!, $itemsNum: Int!, $cursor: String) {
         events: groupByUrlname(urlname: $urlname) {
           unifiedEvents(input: { first: $itemsNum, after: $cursor }) {
@@ -50,69 +49,80 @@ async function fetchEvents(meetupAccessToken) {
       }
     `;
 
-    async function fetchAllFutureEvents(urlname, cursor = null) {
-        const requestBody = JSON.stringify({
-            query: GET_FUTURE_EVENTS,
-            variables: {
-                urlname,
-                itemsNum: batchSize,
-                cursor,
-            },
-        });
+	async function fetchAllFutureEvents(urlname, cursor = null) {
+		const requestBody = JSON.stringify({
+			query: GET_FUTURE_EVENTS,
+			variables: {
+				urlname,
+				itemsNum: batchSize,
+				cursor,
+			},
+		});
 
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${meetupAccessToken}`,
-            },
-            body: requestBody,
-        };
+		const requestOptions = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${meetupAccessToken}`,
+			},
+			body: requestBody,
+		};
 
-        try {
-            const response = await fetch(meetupGraphQlEndpoint, requestOptions);
-            const data = await response.json();
-            const events = data.data.events.unifiedEvents.edges;
+		try {
+			const response = await fetch(meetupGraphQlEndpoint, requestOptions);
+			const data = await response.json();
+			const events = data.data.events.unifiedEvents.edges;
 
-            if (data.data.events.unifiedEvents.pageInfo.hasNextPage) {
-                const nextCursor = data.data.events.unifiedEvents.pageInfo.endCursor;
-                const nextEvents = await fetchAllFutureEvents(urlname, nextCursor);
-                events.push(...nextEvents);
-            }
+			if (data.data.events.unifiedEvents.pageInfo.hasNextPage) {
+				const nextCursor =
+					data.data.events.unifiedEvents.pageInfo.endCursor;
+				const nextEvents = await fetchAllFutureEvents(
+					urlname,
+					nextCursor,
+				);
+				events.push(...nextEvents);
+			}
 
-            return events;
-        } catch (error) {
-            console.error('Error fetching future events:', error);
-            return [];
-        }
-    }
+			return events;
+		} catch (error) {
+			console.error('Error fetching future events:', error);
+			return [];
+		}
+	}
 
-    async function fetchAndPrintAllFutureEvents() {
-        for (const urlname of GROUP_URLNAMES) {
-            const futureEvents = await fetchAllFutureEvents(urlname);
+	async function fetchAndPrintAllFutureEvents() {
+		for (const urlname of GROUP_URLNAMES) {
+			const futureEvents = await fetchAllFutureEvents(urlname);
 
-            futureEvents.forEach(event => {
-                console.log('--------------');
-                console.log('Event:', event.node.title);
-                console.log('Event Link:', event.node.eventUrl);
-                console.log('Description:', event.node.description);
-                console.log('Time:', new Date(event.node.dateTime));
-                console.log('Duration:', event.node.duration);
-                console.log('Location:', event.node.venue.name + '\n'
-                    + event.node.venue.address + '\n'
-                    + event.node.venue.city + ', '
-                    + event.node.venue.state + '\n'
-                    + event.node.venue.postalCode);
-                console.log('Group:', event.node.group.name);
-                console.log('--------------');
-            });
-        }
-    }
+			futureEvents.forEach((event) => {
+				console.log('--------------');
+				console.log('Event:', event.node.title);
+				console.log('Event Link:', event.node.eventUrl);
+				console.log('Description:', event.node.description);
+				console.log('Time:', new Date(event.node.dateTime));
+				console.log('Duration:', event.node.duration);
+				console.log(
+					'Location:',
+					event.node.venue.name +
+						'\n' +
+						event.node.venue.address +
+						'\n' +
+						event.node.venue.city +
+						', ' +
+						event.node.venue.state +
+						'\n' +
+						event.node.venue.postalCode,
+				);
+				console.log('Group:', event.node.group.name);
+				console.log('--------------');
+			});
+		}
+	}
 
-    await fetchAndPrintAllFutureEvents();
+	await fetchAndPrintAllFutureEvents();
 }
 
 export async function handler() {
-    const token = await getMeetupToken();
-    await fetchEvents(token);
+	const token = await getMeetupToken();
+	await fetchEvents(token);
 }
--- lambdas/lib/is-record.ts
+++ lambdas/lib/is-record.ts
@@ -1,3 +1,3 @@
 export function isRecord(value: unknown): value is Record<string, unknown> {
-    return value !== null && typeof value === 'object' && !Array.isArray(value)
+	return value !== null && typeof value === 'object' && !Array.isArray(value);
 }
