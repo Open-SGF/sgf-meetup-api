803a1743f657ad96bc2205f62c3c9471529a4d0f
Levi Zitting <me@levizitting.com>
2025-04-05 22:02:37 -0500

    wip: implementing meetup repository

--- pkg/importer/meetup_repository.go
+++ pkg/importer/meetup_repository.go
@@ -4,7 +4,26 @@ import (
 	"context"
 	"encoding/json"
 	"sgf-meetup-api/pkg/models"
+	"time"
 )
+
+type meetupRepository struct {
+	handler GraphQLHandler
+}
+
+type MeetupRepository interface {
+	GetEventsUntilDateForGroup(ctx context.Context, group string, beforeDate time.Time) ([]models.MeetupEvent, error)
+}
+
+type GraphQLHandler interface {
+	ExecuteQuery(ctx context.Context, query string, variables map[string]any) ([]byte, error)
+}
+
+func NewMeetupRepository(handler GraphQLHandler) MeetupRepository {
+	return &meetupRepository{
+		handler: handler,
+	}
+}
 
 const getFutureEventsQuery = `
   query ($urlname: String!, $itemsNum: Int!, $cursor: String) {
@@ -57,33 +76,59 @@ type MeetupFutureEventsResponse struct {
 					EndCursor   string `json:"endCursor"`
 					HasNextPage bool   `json:"hasNextPage"`
 				} `json:"pageInfo"`
-				Edge struct {
+				Edges []struct {
 					Node models.MeetupEvent `json:"node"`
-				} `json:"edge"`
+				} `json:"edges"`
 			} `json:"unifiedEvents"`
 		} `json:"events"`
 	} `json:"data"`
 }
 
-type meetupRepository struct {
-	handler GraphQLHandler
-}
+func (r *meetupRepository) GetEventsUntilDateForGroup(ctx context.Context, group string, beforeDate time.Time) ([]models.MeetupEvent, error) {
+	events := make([]models.MeetupEvent, 0)
+	cursor := ""
+	var maxFutureDate time.Time
 
-type MeetupRepository interface {
-}
+	for {
+		variables := map[string]any{
+			"urlname": group,
+			"count":   20,
+			"cursor":  cursor,
+		}
 
-type GraphQLHandler interface {
-	ExecuteQuery(ctx context.Context, query string, variables map[string]any) ([]byte, error)
-}
+		response, err := executeGraphQLQuery[MeetupFutureEventsResponse](r, ctx, getFutureEventsQuery, variables)
 
-func NewMeetupRepository(handler GraphQLHandler) MeetupRepository {
-	return &meetupRepository{
-		handler: handler,
+		if err != nil {
+			return nil, err
+		}
+
+		for _, edge := range response.Data.Events.UnifiedEvents.Edges {
+			event := edge.Node
+			events = append(events, event)
+
+			if event.DateTime.After(maxFutureDate) {
+				maxFutureDate = *event.DateTime
+			}
+		}
+
+		if maxFutureDate.After(beforeDate) {
+			break
+		}
+
+		pageInfo := response.Data.Events.UnifiedEvents.PageInfo
+
+		if !pageInfo.HasNextPage {
+			break
+		}
+
+		cursor = pageInfo.EndCursor
 	}
+
+	return events, nil
 }
 
-func executeGraphQLQuery[T any](ctx context.Context, c *meetupRepository, query string, variables map[string]any) (*T, error) {
-	responseBytes, err := c.handler.ExecuteQuery(ctx, query, variables)
+func executeGraphQLQuery[T any](r *meetupRepository, ctx context.Context, query string, variables map[string]any) (*T, error) {
+	responseBytes, err := r.handler.ExecuteQuery(ctx, query, variables)
 
 	if err != nil {
 		return nil, err
@@ -96,10 +141,3 @@ func executeGraphQLQuery[T any](ctx context.Context, c *meetupRepository, query 
 
 	return &response, nil
 }
-
-//func (s *Service) GetEventsForGroup(ctx context.Context, group string) ([]models.MeetupEvent, error) {
-//	//fetchCutOff := time.Now().AddDate(0, 6, 0)
-//	//var newestDate time.Time
-//
-//	//var response :=
-//}
