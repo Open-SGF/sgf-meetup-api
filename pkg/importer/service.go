72817ab93f17f7166a7956d528e1ce4d6ac485d4
Levi Zitting <me@levizitting.com>
2025-04-14 13:33:50 -0500

    chore: cleanup importer service

--- pkg/importer/service.go
+++ pkg/importer/service.go
@@ -2,9 +2,9 @@ package importer
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"sgf-meetup-api/pkg/shared/clock"
-	"sync"
 	"time"
 )
 
@@ -19,11 +19,11 @@ func NewServiceConfig(config *Config) ServiceConfig {
 }
 
 type Service struct {
-	config          ServiceConfig
-	timeSource      clock.TimeSource
-	logger          *slog.Logger
-	eventRepository EventRepository
-	meetupRepo      MeetupRepository
+	config           ServiceConfig
+	timeSource       clock.TimeSource
+	logger           *slog.Logger
+	eventRepository  EventRepository
+	meetupRepository MeetupRepository
 }
 
 func NewService(
@@ -31,44 +31,50 @@ func NewService(
 	timeSource clock.TimeSource,
 	logger *slog.Logger,
 	eventRepository EventRepository,
-	meetupRepo MeetupRepository,
+	meetupRepository MeetupRepository,
 ) *Service {
 	return &Service{
-		config:          config,
-		timeSource:      timeSource,
-		logger:          logger,
-		eventRepository: eventRepository,
-		meetupRepo:      meetupRepo,
+		config:           config,
+		timeSource:       timeSource,
+		logger:           logger,
+		eventRepository:  eventRepository,
+		meetupRepository: meetupRepository,
 	}
 }
 
 func (s *Service) Import(ctx context.Context) error {
 	sixMonthsFromNow := s.timeSource.Now().AddDate(0, 6, 0)
 
-	sem := make(chan struct{}, 3)
-	var wg sync.WaitGroup
+	const maxConcurrency = 3
+	semaphore := make(chan struct{}, maxConcurrency)
+	results := make(chan error, len(s.config.GroupNames))
 
 	for _, group := range s.config.GroupNames {
-		sem <- struct{}{}
-		wg.Add(1)
-
-		go func(g string) {
-			defer func() {
-				<-sem
-				wg.Done()
-			}()
-
-			err := s.importForGroup(ctx, g, sixMonthsFromNow)
-			if err != nil {
-				s.logger.Error("error fetching events", "group", g)
-			} else {
-				s.logger.Info("successfully imported events for group", "group", g)
-			}
-		}(group)
+		semaphore <- struct{}{}
+		go s.importWorker(ctx, group, sixMonthsFromNow, results, semaphore)
 	}
 
-	wg.Wait()
-	return nil
+	var multiErr error
+	for range s.config.GroupNames {
+		if err := <-results; err != nil {
+			multiErr = errors.Join(multiErr, err)
+		}
+	}
+	close(results)
+	return multiErr
+}
+
+func (s *Service) importWorker(ctx context.Context, group string, beforeDate time.Time, results chan<- error, signal <-chan struct{}) {
+	defer func() { <-signal }()
+
+	err := s.importForGroup(ctx, group, beforeDate)
+	if err != nil {
+		s.logger.Error("error fetching events", "group", group)
+		results <- err
+	} else {
+		s.logger.Info("successfully imported events for group", "group", group)
+		results <- nil
+	}
 }
 
 func (s *Service) importForGroup(ctx context.Context, group string, beforeDate time.Time) error {
@@ -79,15 +85,15 @@ func (s *Service) importForGroup(ctx context.Context, group string, beforeDate t
 	}
 
 	missingEventIds := make([]string, 0)
-	incomingEvents, err := s.meetupRepo.GetEventsUntilDateForGroup(ctx, group, beforeDate)
+	incomingEvents, err := s.meetupRepository.GetEventsUntilDateForGroup(ctx, group, beforeDate)
 
 	if err != nil {
 		return err
 	}
 
-	incomingEventIds := make(map[string]bool, len(incomingEvents))
+	incomingEventIds := make(map[string]struct{}, len(incomingEvents))
 	for _, incomingEvent := range incomingEvents {
-		incomingEventIds[incomingEvent.ID] = true
+		incomingEventIds[incomingEvent.ID] = struct{}{}
 	}
 
 	for _, savedEvent := range savedEvents {
